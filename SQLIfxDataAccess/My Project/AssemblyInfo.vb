ffc373f34150aa24ad1ed25ea9d1a7b879665e79
Administrador <Administrador@SRV-DESARROLLO>
2021-07-22 15:46:32 -0500

    Agregar archivos de proyecto.

---
+++ SQLIfxDataAccess/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("SQLDataAccess")>
+<Assembly: AssemblyDescription("Dll de conexión y recuperación de datos para SQL Server")>
+<Assembly: AssemblyCompany("Edgar Orozco Guayara")>
+<Assembly: AssemblyProduct("SQLDataAccess")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("Edgar Orozco Guayara")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("08727731-9be3-4e04-bf95-3fc410c27453")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.2")>
+<Assembly: AssemblyFileVersion("1.0.0.2")>
